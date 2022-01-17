f16abbc963a1ad7371062cba059307fa61309a9e
andreashsy <songyanghan+git@gmail.com>
2022-01-17 11:09:56 +0800

    changed api to path variable

--- src/main/java/weatherapi/firstversion/controllers/RestWeatherController.java
+++ src/main/java/weatherapi/firstversion/controllers/RestWeatherController.java
@@ -5,8 +5,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import weatherapi.firstversion.service.WeatherService;
@@ -17,8 +17,8 @@ public class RestWeatherController {
     @Autowired
     private WeatherService weatherService;
 
-    @GetMapping
-    public ResponseEntity<String> cityFormAPI(@RequestParam(required=true) String city, Model model) {
+    @GetMapping(path="{city}")
+    public ResponseEntity<String> cityFormAPI(@PathVariable String city, Model model) {
         String jsonData = weatherService.getWeather(city);
         return ResponseEntity.ok(jsonData);
     }
